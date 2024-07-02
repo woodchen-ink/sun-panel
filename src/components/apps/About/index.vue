9cc3586f81ed0425e3442d0f74267bdc52a7e7c9
wood <wood@czl.net>
2024-07-02 16:23:03 +0800

    修

--- src/components/apps/About/index.vue
+++ src/components/apps/About/index.vue
@@ -1,33 +1,7 @@
 <script setup lang="ts">
-import { NDivider, NGradientText, NTag } from 'naive-ui'
-import { onMounted, ref } from 'vue'
-import { get } from '@/api/system/about'
-import { useAppStore } from '@/store'
-import { RoundCardModal } from '@/components/common'
 
-interface Version {
-  versionName: string
-  versionCode: number
-}
-
-const appStore = useAppStore()
-const versionName = ref('')
-const qqGroupQRShow = ref(false)
-const frontVersion = import.meta.env.VITE_APP_VERSION || 'unknown'
-
-onMounted(() => {
-  get<Version>().then((res) => {
-    if (res.code === 0)
-      versionName.value = res.data.versionName
-  })
-})
 </script>
 
 <template>
 </template>
 
-<style>
-.link{
-    color:rgb(0, 89, 255)
-}
-</style>
